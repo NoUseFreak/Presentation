67b2b28ca9aff2149d26ba1920fe1fd4a98f13fc
Dries De Peuter <dries@nousefreak.be>
2013-10-27 17:30:45 +0100

    Refactor into classes

--- web/js/presentation.js
+++ web/js/presentation.js
@@ -1,42 +1,70 @@
 
-var sess;
-var api;
-var slide = {
-    disableControls: true
-};
-document.addEventListener("keyup", function ( event ) {
-    if (slide.disableControls) {
-        event.stopPropagation();
-        event.stopImmediatePropagation();
-        return false;
+var PresentationClient = function() {
+
+    var conn;
+    var session;
+    var api;
+    var settings = {
+        'disableControls': true
+    };
+
+    var init = function() {
+        init_impressjs();
+        init_connection();
+        init_controls();
     }
-});
 
-$(function() {
-    api = impress();
-    api.init();
-    api.goto(0);
-});
+    var init_connection = function() {
+        conn = new ab.connect(
+            'ws://' + wsHost
+            , function(s) {
+                session = s;
 
+                session.call('getPosition', {total: api.step.count()}).then(function(data) {
+                    api.goto(data.position);
+                });
 
+                session.subscribe('presentationControl', function(topic, data) {
+                    api.goto(data.position);
+                });
+            }
+            , function(code, reason) {
+                console.warn('WebSocket connection closed', code, reason);
+            }
+            , {
+                'skipSubprotocolCheck': true
+            }
+        );
+    }
 
-var conn = new ab.connect(
-    'ws://' + wsHost
-    , function(session) {
-        sess = session;
-
-        sess.call('getPosition', {total: api.step.count()}).then(function(data) {
-            api.goto(data.position);
+    var init_impressjs = function() {
+        $(function() {
+            api = impress();
+            api.init();
+            api.goto(0);
         });
+    }
 
-        sess.subscribe('presentationControl', function(topic, data) {
-            api.goto(data.position);
+    var init_controls = function() {
+        document.addEventListener("keyup", function ( event ) {
+            if (settings.disableControls) {
+                event.stopPropagation();
+                event.stopImmediatePropagation();
+                return false;
+            }
         });
     }
-    , function(code, reason) {
-        console.warn('WebSocket connection closed', code, reason);
+
+    var toggleControls = function() {
+        settings.disableControls = !settings.disableControls;
     }
-    , {
-        'skipSubprotocolCheck': true
-    }
-);
+
+    return {
+        'init': init,
+        'toggleControls': toggleControls
+    };
+}
+
+var press = new PresentationClient();
+press.init();
+
--- web/js/presentation_master.js
+++ web/js/presentation_master.js
@@ -1,59 +1,88 @@
-var sess;
-var conn = new ab.connect(
-    'ws://' + wsHost
-    , function(session) {
-        sess = session;
+var PresentationMaster = function() {
 
-        sess.call('getPosition').then(function(data) {
+    var session;
+    var conn;
+
+    var init = function() {
+        init_connection();
+    }
+
+    var init_connection = function() {
+        conn = new ab.connect(
+            'ws://' + wsHost
+            , function(s) {
+                session = s;
+
+                session.call('getPosition').then(function(data) {
+                    slideCount(data.position + 1);
+                });
+            }
+            , function(code, reason) {
+                console.warn('WebSocket connection closed', code, reason);
+            }
+            , {
+                'skipSubprotocolCheck': true
+            }
+        );
+    }
+
+    var next = function(callback) {
+        session.call('presentationControl', {action: 'next'}).then(function(data) {
+            if (callback) {
+                callback(data.position);
+            }
             slideCount(data.position + 1);
         });
     }
-    , function(code, reason) {
-        console.warn('WebSocket connection closed', code, reason);
-    }
-    , {
-        'skipSubprotocolCheck': true
-    }
-);
 
-function slideNext() {
-    sess.call('presentationControl', {action: 'next'}).then(function(data) {
-        slideCount(data.position + 1);
-    });
-}
-function slidePrev() {
-    sess.call('presentationControl', {action: 'prev'}).then(function(data) {
-        slideCount(data.position + 1);
-    });
-}
-function slideCount(count) {
-    if (count == undefined) {
-        return parseInt($('div.slide-count').text());
+    var prev = function(callback) {
+        session.call('presentationControl', {action: 'prev'}).then(function(data) {
+            if (callback) {
+                callback(data.position);
+            }
+            slideCount(data.position + 1);
+        });
     }
-    $('div.slide-count').text(count);
+
+    return {
+        'init': init,
+        'next': next,
+        'prev': prev
+    };
 }
+
+var press = new PresentationMaster();
+press.init();
 
 $(function() {
+    function slideCount(count) {
+        $('div.slide-count').text(count);
+    }
+
     $('body').on('keydown', function(event) {
         switch (event.keyCode) {
             case 9:
             case 32:
             case 39:
             case 40:
-                slideNext();
+                press.next();
                 return false;
 
             case 37:
             case 38:
-                slidePrev();
+                press.prev();
                 return false;
         }
     });
     $('a.slide-next').on('click', function() {
-        slideNext();
+        press.next(function(count) {
+            slideCount(count+1);
+        });
     })
     $('a.slide-prev').on('click', function() {
-        slidePrev();
+        press.prev(function(count) {
+            slideCount(count+1);
+        });
     })
     $('a.control').on('click', function() {
         $(this).toggleClass('on');
@@ -65,3 +94,4 @@ $(function() {
         }
     })
 });
+
